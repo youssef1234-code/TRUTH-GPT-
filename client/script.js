8168243434e858a842b572235a4427848199b1da
unknown <youssef.mohamed1407@gmail.com>
2023-02-28 00:31:42 +0200

    SECOND commit

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async(e)=>{
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://truth-gpt-v1-0.onrender.com',{
     method: 'POST',
   headers:{
     'Content-Type': 'application/json'  
